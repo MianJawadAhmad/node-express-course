1fbc69fde87d568ee5aa474cee18adf0af667ee1
MianJawadAhmad <a.jawad321@gmail.com>
2020-08-12 12:41:55 +0500

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
 
 const mockUserData=[
 	{name:'Mark'},
@@ -13,7 +16,7 @@ app.get('/users',function(req,res){
 		users: mockUserData
 	})
 })
-
+// colons are used as variables that be viewed in the params
 app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
 	res.json({
@@ -21,6 +24,31 @@ app.get('/users/:id',function(req,res){
 		message: 'got one user',
 		user: req.params.id
 	})
+})
+
+app.post('/login',function(req,res){
+	// Typically passwords are encrypted using something like bcrypt before sending to database
+	const username=req.body.username;
+	const password=req.body.password;
+
+	// This should come from the database
+	const mockUsername="billyTheKid";
+	const mockPassword="superSecret";
+
+	if (username===mockUsername && password===mockPassword){
+		// In practice, use JSON web token sign method here to make an encrypted token
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
+	} else {
+		res.json({
+			success: false,
+			message: 'password and username do not match'
+		})
+	}
+
 })
 
 app.listen(8000,function(){console.log('server is listening')})
